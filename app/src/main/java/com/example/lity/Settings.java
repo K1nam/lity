90f82f76989a3aa6e1c9b40b61bbecc41ff53dad
K1nam <nuneslucas178@gmail.com>
2024-11-13 22:29:58 -0300

    app

--- app/src/main/java/com/example/lity/Settings.java
+++ app/src/main/java/com/example/lity/Settings.java
@@ -1,5 +1,7 @@
 package com.example.lity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -20,15 +22,26 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         iniciarcomponentes();
+        configurarAcoes();
     }
-    private void iniciarcomponentes(){
+    private void iniciarcomponentes() {
         bt_git = findViewById(R.id.bt_git);
         back = findViewById(R.id.back);
+    }
+    private void configurarAcoes() {
+        bt_git.setOnClickListener(view -> {
+            String url = "https://github.com/K1nam/lity";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+        back.setOnClickListener(view -> finish());
     }
 }
